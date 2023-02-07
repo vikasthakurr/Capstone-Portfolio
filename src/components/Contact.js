affff0bfa4d519bd0acdfdf0ae1e3b9d3f7f3d01
vikasthakurr <vikaskumar20012001@gmail.com>
2023-02-08 02:44:25 +0530

    navbar css changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import tele from "../images/telegramicon.png"
 const Contact = () => {
   return (
     <>
-      <div className="contact">
+      <div className="contact" id="contact">
       <img src={tele} alt="" />
         <a href="https://www.linkedin.com/in/vikasthakurr/">Get Me On Linkedin</a>
          
